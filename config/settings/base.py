0601d87dee649ac05702aa628a98894adf089017
nasimul <nasimul.hasan.aiub@gmail.com>
2026-07-11 11:26:18 +0600

    Added custom pagination class

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,7 +89,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "config.pagination.CustomPageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
